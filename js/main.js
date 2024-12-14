04504949a6537f52f056694d36988be159664eb0
Jesús José Cantero López <118011169+TheYesua@users.noreply.github.com>
2024-12-14 19:38:58 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+function openPopup(url, title, width, height) {
+    const left = (screen.width / 2) - (width / 2);
+    const top = (screen.height / 2) - (height / 2);
+    window.open(url, title, `width=${width},height=${height},top=${top},left=${left},resizable=yes`);
+}
