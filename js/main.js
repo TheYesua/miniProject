3c221cda3def995f274f2d89db79ac9a74bb8344
Jesús José Cantero López <118011169+TheYesua@users.noreply.github.com>
2024-12-20 20:30:22 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ function openPopup(url, title, width, height) {
     const top = (screen.height / 2) - (height / 2);
     window.open(url, title, `width=${width},height=${height},top=${top},left=${left},resizable=yes`);
 }
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const date = now.toLocaleDateString();
+
+    const clock = document.getElementById('digital-clock');
+    clock.textContent = `${date} ${hours}:${minutes}:${seconds}`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
